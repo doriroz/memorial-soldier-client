b3a7edadd446c22b1a4a6ce15eeb1ecac9bf1b3a
doriroz <doriroz@gmail.com>
2024-02-12 19:12:23 +0200

    change npm start cmd for start front and back together

--- src/Backend/HeroDBRouting.js
+++ src/Backend/HeroDBRouting.js
@@ -21,9 +21,6 @@ router.post("/notes/addnote", async (req, res) => {
     const result = await note.save();
     console.log(result);
     res.status(201).json({ message: "note added succefully" });
-    // res.setHeader("Cache-Control", "no-store, max-age=0");
-    // res.status(301);
-    // res.redirect("www.google.com");
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: "Internal Error" });
