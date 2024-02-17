3e089ffbfcc432e49f221675a37b2bd1f5d98e0a
doriroz <doriroz@gmail.com>
2024-02-17 12:55:00 +0200

    test3

--- src/Pages/AddNote.jsx
+++ src/Pages/AddNote.jsx
@@ -13,10 +13,8 @@ const AddNote = () => {
     console.log("contactRef : " + contactRef.current.value);
     console.log("contextRef : " + contextRef.current.value);
     if (contactRef.current.value != "" && contextRef.current.value != "") {
-      // console.log("disabled false");
       setIsDisabled(false);
     } else {
-      // console.log("disabled true");
       setIsDisabled(true);
     }
   };
