26e3081c43eb1513cf8cf40e5c71add300105451
doriroz <doriroz@gmail.com>
2024-02-17 12:14:29 +0200

    add useeffect for ref prev button element

--- src/Component/Carusel.jsx
+++ src/Component/Carusel.jsx
@@ -82,10 +82,14 @@ const Carusel = (props) => {
       console.log(notesArray);
       setNotesData(notesArray);
       setIsLoading(false);
-      prevRef.current.style.opacity = 0;
+      // prevRef.current.style.opacity = 0;
     };
     fetchNotes();
   }, []);
+
+  useEffect(() => {
+    prevRef.current.style.opacity = 0;
+  }, [isLoading]);
 
   const prevHandler = () => {
     const newIndex = Math.max(index - 1, 0);
